10587196d09e5c4e6586fa4009ecc32a071c12a8
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-10 08:54:14 -0300

    fix: remove consol.log

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,52 +1,50 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
 export interface DropdownValue {
-    value: string;
-    func: () => void;
-    active: boolean;
+  value: string;
+  func: () => void;
+  active: boolean;
 }
 
 @Component({
-    selector: 'dropdown',
-    standalone: true,
-    imports: [CommonModule],
-    template: `
-  <div class="dropdownContainer">
-    <button (click)="changeOpenStatus()">Colunas</button>
-    <ul *ngIf="isOpen">
-        <li *ngFor="let val of getValues(values)" (click)="changeColumnStatus(val[0])">
-            {{val[0]}}
+  selector: "dropdown",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="dropdownContainer">
+      <button (click)="changeOpenStatus()">Colunas</button>
+      <ul *ngIf="isOpen">
+        <li
+          *ngFor="let val of getValues(values)"
+          (click)="changeColumnStatus(val[0])"
+        >
+          {{ val[0] }}
         </li>
-    </ul>
-  </div>
+      </ul>
+    </div>
   `,
-    styleUrl: './dropdown.component.scss'
+  styleUrl: "./dropdown.component.scss",
 })
 export class DropdownComponent {
-    isOpen: boolean = false;
+  isOpen: boolean = false;
 
-    @Input()
-    //values: DropdownValue[] = [];
-    values: Map<string,boolean> = new Map<string,boolean>();
+  @Input()
+  values: Map<string, boolean> = new Map<string, boolean>();
 
+  constructor() {}
 
-    constructor() {
-    }
+  changeColumnStatus(key: string) {
+    if (!this.values.has(key)) return;
+    const val = this.values.get(key);
+    this.values.set(key, !val);
+  }
 
-    changeColumnStatus(key:string){
-        if (!this.values.has(key)) return
-            const val = this.values.get(key);
-        this.values.set(key,!val);
-        console.log(this.values)
-    }
+  changeOpenStatus() {
+    this.isOpen = !this.isOpen;
+  }
 
-    changeOpenStatus() {
-        this.isOpen = !this.isOpen
-    }
-
-    getValues(map:Map<string,boolean>){
-        return Array.from(map.entries())
-    }
-
+  getValues(map: Map<string, boolean>) {
+    return Array.from(map.entries());
+  }
 }
